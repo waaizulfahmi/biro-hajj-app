019dc306cdb2f2df80a8573e0ca993a6d1cab19e
waaizulfahmi <wandaaizulf@gmail.com>
2024-02-08 14:04:53 +0700

    update

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
-    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
+    {{-- <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}"> --}}
     <title>
         {{ env('APP_NAME') }} - @yield('title', 'Login')
     </title>
@@ -14,7 +14,7 @@
 
     {{-- Nucleo Icons --}}
     <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />       
     {{-- Font Awesome Icons --}}
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
